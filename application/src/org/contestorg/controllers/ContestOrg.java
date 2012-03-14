f3b889978956ddfd1b1ee4fd641c6f98e06eaca2
perrin.cyril.88 <perrin.cyril.88@gmail.com>
2012-03-14 19:03:31 +0000

    Ajout de la description du numéro de version : [version principale].[fonctionnalités développées].[anomalies corrigées]

--- application/src/org/contestorg/controllers/ContestOrg.java
+++ application/src/org/contestorg/controllers/ContestOrg.java
@@ -50,7 +50,7 @@ import org.contestorg.views.ViewHelper;
 public class ContestOrg extends MoodyAbstract implements IHistoryListener
 {
 	
-	/** Version de ContestOrg */
+	/** Version de ContestOrg ([version principale].[fonctionnalités développées].[anomalies corrigées]) */
 	public static final String VERSION = "2.0.0b";  
 
 	/** Instance unique de ContestOrg */
