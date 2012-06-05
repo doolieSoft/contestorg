134248f2be43e7197b913afcdf04d4a9aae7de37
perrin.cyril.88@gmail.com <perrin.cyril.88@gmail.com@1819b853-7887-72f3-d475-8e13ee262973>
2012-06-05 06:11:46 +0000

    Prise en compte des dernières modifications dans le numéro de version et remise à zéro du nombre d'anomalies car le nombre d'évolutions a augmenté

--- trunk/application/src/org/contestorg/controllers/ContestOrg.java
+++ trunk/application/src/org/contestorg/controllers/ContestOrg.java
@@ -53,7 +53,7 @@ public class ContestOrg extends MoodyAbstract implements IHistoryListener
 {
 	
 	/** Version de ContestOrg ([version principale].[évolutions développées].[anomalies corrigées]) */
-	public static final String VERSION = "2.1.1";
+	public static final String VERSION = "2.1.0";
 
 	/** Instance unique de ContestOrg */
 	private static ContestOrg contestOrg;
