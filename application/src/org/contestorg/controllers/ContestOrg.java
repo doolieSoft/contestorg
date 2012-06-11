70a36465cf08740e9e4eec624e5e729c0d7e10fa
perrin.cyril.88 <perrin.cyril.88@gmail.com>
2012-06-11 13:28:35 +0000

    Prise en compte des dernières modifications dans le fichier "lisez-moi.html" et numéros de version

--- application/src/org/contestorg/controllers/ContestOrg.java
+++ application/src/org/contestorg/controllers/ContestOrg.java
@@ -53,7 +53,7 @@ public class ContestOrg extends MoodyAbstract implements IHistoryListener
 {
 	
 	/** Version de ContestOrg ([version principale].[évolutions développées].[anomalies corrigées]) */
-	public static final String VERSION = "2.1.0";
+	public static final String VERSION = "2.2.0";
 
 	/** Instance unique de ContestOrg */
 	private static ContestOrg contestOrg;
