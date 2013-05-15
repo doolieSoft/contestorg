a67c45f296c355c7df54bde6e9762949a3bc6eb7
perrin.cyril.88 <perrin.cyril.88@gmail.com>
2013-05-15 19:56:10 +0000

    Issue #109 : Documentation au format d'aide

--- application/make/generer-doc/generer-doc.bat
+++ application/make/generer-doc/generer-doc.bat
@@ -1,14 +1,23 @@
 @echo OFF
 
-REM Générer la documentation de l'application en PDF et en HTML
+REM Générer la documentation CHM
+mkdir temp
+cd temp
+mkdir images
+xcopy /e ..\..\..\doc\images .\images
+java -jar ..\..\..\lib\saxon-6.5.5\saxon.jar ..\..\..\doc\documentation.xml ..\docbook-xsl-1.76.1\htmlhelp\htmlhelp.xsl
+hhc htmlhelp
+copy htmlhelp.chm ..\..\..\doc\documentation.chm
+cd ..
+rmdir /s /q temp
 
 REM Se rendre dans le répertoire de la documentation
 cd ..\..\doc
 
 REM Générer la documentation HTML
-java -jar ..\lib\saxon-9he\saxon.jar documentation.xml ..\make\generer-doc\docbook-xsl-1.76.1\html\docbook.xsl > documentation.html
+java -jar ..\lib\saxon-6.5.5\saxon.jar documentation.xml ..\make\generer-doc\docbook-xsl-1.76.1\html\docbook.xsl > documentation.html
 
 REM Générer la documentation PDF
-java -jar ..\lib\saxon-9he\saxon.jar documentation.xml ..\make\generer-doc\docbook-xsl-1.76.1\fo\docbook.xsl > documentation.fo
+java -jar ..\lib\saxon-6.5.5\saxon.jar documentation.xml ..\make\generer-doc\docbook-xsl-1.76.1\fo\docbook.xsl > documentation.fo
 java -jar ..\lib\fop-1.0\fop-1.0.jar documentation.fo -pdf documentation.pdf
 del documentation.fo
