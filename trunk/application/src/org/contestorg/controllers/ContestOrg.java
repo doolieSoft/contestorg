64444b13da3a7f890b5d99de416e0fe0942b643b
perrin.cyril.88@gmail.com <perrin.cyril.88@gmail.com@1819b853-7887-72f3-d475-8e13ee262973>
2013-05-15 19:44:17 +0000

    Simplification de l'explication du numéro de version

--- trunk/application/src/org/contestorg/controllers/ContestOrg.java
+++ trunk/application/src/org/contestorg/controllers/ContestOrg.java
@@ -50,7 +50,7 @@ import org.contestorg.views.ViewHelper;
 public class ContestOrg extends MoodyAbstract implements IHistoryListener
 {
 	
-	/** Version de ContestOrg ([version principale].[évolutions développées].[anomalies corrigées]) */
+	/** Version de ContestOrg ([évolution majeure].[évolution mineure].[correction]) */
 	public static final String VERSION = "2.3.1";
 
 	/** Instance unique de ContestOrg */
