4e85d11016ced48ba05fbbeeaedf37695ca73974
perrin.cyril.88@gmail.com <perrin.cyril.88@gmail.com@1819b853-7887-72f3-d475-8e13ee262973>
2013-11-10 10:21:13 +0000

    Issue #95 : Combiner les évolutions si il y a plusieurs versions d'écart

--- trunk/application/src/org/contestorg/out/MiseAJour.java
+++ trunk/application/src/org/contestorg/out/MiseAJour.java
@@ -3,12 +3,14 @@ package org.contestorg.out;
 import java.io.StringReader;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Comparator;
 import java.util.Iterator;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.util.EntityUtils;
+import org.contestorg.comparators.CompVersions;
 import org.contestorg.controllers.ContestOrg;
 import org.contestorg.infos.InfosMiseAJour;
 import org.contestorg.infos.InfosMiseAJour.Modification;
@@ -55,9 +57,12 @@ public class MiseAJour
 				// Retenir la date de vérification des mises à jour
 				preferences.setAndSave(Preferences.DERNIERE_VERIFICATION_MISES_A_JOUR, timestamp);
 				
+				// Message de log
+				Log.getLogger().info("Vérification des mises à jour...");
+				
 				// Récupérer la liste des mises à jour
 				HTTPHelper.Browser browser = new HTTPHelper.Browser();
-				HttpResponse response = browser.get("http://www.elfangels.fr", 80, "/contestorg/api/versions");
+				HttpResponse response = browser.get("http://localhost", 80, "/Projets/ContestOrg/api/versions");
 				
 				// Vérifier si la requête a réussie
 				if(response == null || response.getStatusLine() == null || response.getStatusLine().getStatusCode() != 200) {
@@ -90,13 +95,23 @@ public class MiseAJour
 					return null;
 				}
 				
-				// Vérifier si la dernière version est la même que celle actuelle
-				if(elementVersion.getAttributeValue("numero").equals(ContestOrg.VERSION)) {
+				// Comparateur de versions
+				Comparator<String> comparateurVersions = new CompVersions();
+				
+				// Récupérer le numéro de version
+				String numero = elementVersion.getAttributeValue("numero");
+				
+				// Vérifier si la dernière version est supérieure à la version actuelle
+				if(comparateurVersions.compare(numero, ContestOrg.VERSION) < 0) {
+					// Message de log
+					Log.getLogger().info("Pas de nouvelles mises à jour depuis la "+ContestOrg.VERSION);
+					
+					// Retourner null
 					return null;
 				}
 				
-				// Récupérer le nom
-				String numero = elementVersion.getAttributeValue("numero");
+				// Message de log
+				Log.getLogger().info("Nouvelle version "+numero+" disponible");
 				
 				// Récupérer et parser la date de publication
 				Matcher matcher = Pattern.compile("([0-9]{2,2})/([0-9]{2,2})/([0-9]{4,4})").matcher(elementVersion.getAttributeValue("date"));
@@ -121,55 +136,66 @@ public class MiseAJour
 					}
 				}
 				
-				// Récupérer la liste des évolutions développées
+				// Initialiser les listes de modifications
 				ArrayList<Modification> evolutions = new ArrayList<Modification>();
-				if(elementVersion.getChild("listeEvolutions") != null) {
-					Iterator iteratorEvolutions = elementVersion.getChild("listeEvolutions").getChildren().iterator();
-					while (iteratorEvolutions.hasNext()) {
-						Element evolution = (Element)iteratorEvolutions.next();
-						evolutions.add(new Modification(
-							evolution.getAttributeValue("description"),
-							evolution.getAttributeValue("ticket")
-						));
-					}
-				}
-				
-				// Récupérer la liste des anomalies corrigées
 				ArrayList<Modification> anomalies = new ArrayList<Modification>();
-				if(elementVersion.getChild("listeAnomalies") != null) {
-					Iterator iteratorAnomalies = elementVersion.getChild("listeAnomalies").getChildren().iterator();
-					while (iteratorAnomalies.hasNext()) {
-						Element evolution = (Element)iteratorAnomalies.next();
-						anomalies.add(new Modification(
-							evolution.getAttributeValue("description"),
-							evolution.getAttributeValue("ticket")
-						));
-					}
-				}
-				
-				// Récupérer la liste des revues effectuées
 				ArrayList<Modification> revues = new ArrayList<Modification>();
-				if(elementVersion.getChild("listeRevues") != null) {
-					Iterator iteratorRevues = elementVersion.getChild("listeRevues").getChildren().iterator();
-					while (iteratorRevues.hasNext()) {
-						Element evolution = (Element)iteratorRevues.next();
-						revues.add(new Modification(
-							evolution.getAttributeValue("description"),
-							evolution.getAttributeValue("ticket")
-						));
-					}
-				}
-				
-				// Récupérer la liste des tâches effectuées
 				ArrayList<Modification> taches = new ArrayList<Modification>();
-				if(elementVersion.getChild("listeTaches") != null) {
-					Iterator iteratorTaches = elementVersion.getChild("listeTaches").getChildren().iterator();
-					while (iteratorTaches.hasNext()) {
-						Element evolution = (Element)iteratorTaches.next();
-						taches.add(new Modification(
-							evolution.getAttributeValue("description"),
-							evolution.getAttributeValue("ticket")
-						));
+				
+				// Récupérer la liste des versions
+				Iterator iteratorVersions = document.getRootElement().getChildren().iterator();
+				while (iteratorVersions.hasNext()) {
+					elementVersion = (Element)iteratorVersions.next();
+					
+					// Vérifier si la version est supérieure à la version actuelle
+					if(comparateurVersions.compare(elementVersion.getAttributeValue("numero"), ContestOrg.VERSION) > 0) {
+						// Récupérer la liste des évolutions développées
+						if(elementVersion.getChild("listeEvolutions") != null) {
+							Iterator iteratorEvolutions = elementVersion.getChild("listeEvolutions").getChildren().iterator();
+							while (iteratorEvolutions.hasNext()) {
+								Element evolution = (Element)iteratorEvolutions.next();
+								evolutions.add(new Modification(
+									evolution.getAttributeValue("description"),
+									evolution.getAttributeValue("ticket")
+								));
+							}
+						}
+						
+						// Récupérer la liste des anomalies corrigées
+						if(elementVersion.getChild("listeAnomalies") != null) {
+							Iterator iteratorAnomalies = elementVersion.getChild("listeAnomalies").getChildren().iterator();
+							while (iteratorAnomalies.hasNext()) {
+								Element evolution = (Element)iteratorAnomalies.next();
+								anomalies.add(new Modification(
+									evolution.getAttributeValue("description"),
+									evolution.getAttributeValue("ticket")
+								));
+							}
+						}
+						
+						// Récupérer la liste des revues effectuées
+						if(elementVersion.getChild("listeRevues") != null) {
+							Iterator iteratorRevues = elementVersion.getChild("listeRevues").getChildren().iterator();
+							while (iteratorRevues.hasNext()) {
+								Element evolution = (Element)iteratorRevues.next();
+								revues.add(new Modification(
+									evolution.getAttributeValue("description"),
+									evolution.getAttributeValue("ticket")
+								));
+							}
+						}
+						
+						// Récupérer la liste des tâches effectuées
+						if(elementVersion.getChild("listeTaches") != null) {
+							Iterator iteratorTaches = elementVersion.getChild("listeTaches").getChildren().iterator();
+							while (iteratorTaches.hasNext()) {
+								Element evolution = (Element)iteratorTaches.next();
+								taches.add(new Modification(
+									evolution.getAttributeValue("description"),
+									evolution.getAttributeValue("ticket")
+								));
+							}
+						}
 					}
 				}
 				
--- trunk/application/src/org/contestorg/out/PersistanceXML.java
+++ trunk/application/src/org/contestorg/out/PersistanceXML.java
@@ -18,6 +18,7 @@ import org.contestorg.common.Pair;
 import org.contestorg.common.Tools;
 import org.contestorg.comparators.CompPhasesElims;
 import org.contestorg.comparators.CompPhasesQualifs;
+import org.contestorg.comparators.CompVersions;
 import org.contestorg.controllers.ContestOrg;
 import org.contestorg.infos.InfosConnexionFTP;
 import org.contestorg.infos.InfosModelCategorie;
@@ -634,72 +635,7 @@ public class PersistanceXML extends PersistanceAbstract
 		// Vérifier si la rétro-compatibilité est nécéssaire
 		if(!ContestOrg.VERSION.equals(version)) {
 			// Comparateur de feuilles de style XSL
-			final Comparator<String> comparateur = new Comparator<String>() {
-				/**
-				 * @see Comparator#compare(Object,Object)
-				 */
-				@Override
-				public int compare (String versionA, String versionB) {
-					// Vérifier si les versions sont vides
-					boolean versionAVide = versionA == null || versionA.isEmpty();
-					boolean versionBVide = versionB == null || versionB.isEmpty();
-					if(!versionAVide && versionBVide) {
-						return 1;
-					}
-					if(versionAVide && !versionBVide) {
-						return -1;
-					}
-					if(versionAVide && versionBVide) {
-						return 0;
-					}
-					
-					// Extraire les informations de la version A
-					String[] informationsVersionA = versionA.split("\\.");
-					int versionPrincipaleA = -1;
-					try {
-						versionPrincipaleA = Integer.parseInt(informationsVersionA[0]);
-					} catch(NumberFormatException e) {}
-					int fonctionnalitesDeveloppeesA = -1;
-					try {
-						fonctionnalitesDeveloppeesA = Integer.parseInt(informationsVersionA[1]);
-					} catch(NumberFormatException e) {}
-					int anomaliesCorrigeesA = -1;
-					try {
-						anomaliesCorrigeesA = Integer.parseInt(informationsVersionA[2]);
-					} catch(NumberFormatException e) {}
-	
-					// Extraire les informations de la version B
-					String[] informationsVersionB = versionB.split("\\.");
-					int versionPrincipaleB = -1;
-					try {
-						versionPrincipaleB = Integer.parseInt(informationsVersionB[0]);
-					} catch(NumberFormatException e) {}
-					int fonctionnalitesDeveloppeesB = -1;
-					try {
-						fonctionnalitesDeveloppeesB = Integer.parseInt(informationsVersionB[1]);
-					} catch(NumberFormatException e) {}
-					int anomaliesCorrigeesB = -1;
-					try {
-						anomaliesCorrigeesB = Integer.parseInt(informationsVersionB[2]);
-					} catch(NumberFormatException e) {}
-					
-					// Comparer les information de version
-					if(versionPrincipaleA == versionPrincipaleB) {
-						if(fonctionnalitesDeveloppeesA == fonctionnalitesDeveloppeesB) {
-							if(anomaliesCorrigeesA == anomaliesCorrigeesB) {
-								return 0;
-							} else {
-								return anomaliesCorrigeesA < anomaliesCorrigeesB ? -1 : 1;
-							}
-						} else {
-							return fonctionnalitesDeveloppeesA < fonctionnalitesDeveloppeesB ? -1 : 1;
-						}
-					} else {
-						return versionPrincipaleA < versionPrincipaleB ? -1 : 1;
-					}
-				}
-				
-			};
+			final Comparator<String> comparateur = new CompVersions();
 			
 			// Récupérer la liste des feuilles de style XSL
 			ArrayList<File> xsls = new ArrayList<File>();
