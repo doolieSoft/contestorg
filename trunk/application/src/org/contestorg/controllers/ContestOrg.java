2c87144bca4985a111cc50dc958f8a95e2394bcb
perrin.cyril.88@gmail.com <perrin.cyril.88@gmail.com@1819b853-7887-72f3-d475-8e13ee262973>
2014-12-16 18:50:17 +0000

    Version 2.5.0

--- trunk/application/src/org/contestorg/controllers/ContestOrg.java
+++ trunk/application/src/org/contestorg/controllers/ContestOrg.java
@@ -46,7 +46,7 @@ public class ContestOrg extends MoodyAbstract implements IHistoryListener
 {
 	
 	/** Version de ContestOrg ([évolution majeure].[évolution mineure].[correction]) */
-	public static final String VERSION = "2.4.2";
+	public static final String VERSION = "2.5.0";
 
 	/** Unique instance de ContestOrg */
 	private static ContestOrg contestOrg;
